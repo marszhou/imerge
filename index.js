2709000143b524e185525ab80684795cb5c174ec
mars <mars@huoban.com>
2016-11-09 14:59:48 +0800

    handle command line params

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-var program = require('commander');
+const program = require('commander')
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const ImageMerge = require('./src/image-merge')
 
 program
   .version('0.0.1')
@@ -10,8 +14,42 @@ program
   .option('-d, --direction <type>', 'indicate merge image direction, option values: [horizontal, vertical], default is [vertical]', /^(horizontal|vertical)$/i,'vertical')
   .option('--dir <dir>', 'input directory')
   .option('--background <color>', 'fill background with [0x000000]', '0x000000')
-  .action((file) => {
-    console.log(file, program.output, program.direction)
+  .action((_files) => {
+    // console.log(file, program.output, program.direction)
+    let files = []
+    let errors = []
+    if (_files.length > 0) {
+      _files.forEach(file => {
+        let _path = file
+        if (!path.isAbsolute(_path)) {
+          _path = path.normalize(path.join(__dirname, _path))
+        }
+        let _exists = fs.existsSync(_path)
+        if (_exists) {
+          files.push(_path)
+        } else {
+          errors.push(chalk.bold.red('File not exists: ') + _path)
+        }
+      })
+    }
+
+    let dir = program.dir
+    if (dir) {
+      dir = path.isAbsolute(dir) ? path.normalize(dir) : path.normalize(path.join(__dirname, dir))
+      if (!fs.existsSync(dir)) {
+        errors.push(chalk.bold.red('Directory not exists: ') + dir)
+      } else {
+        files = files.concat(fs.readdirSync(dir).map(file => path.join(dir, file)))
+      }
+    }
+
+    if (errors.length > 0) {
+      console.log(errors.join('\n'))
+      program.outputHelp()
+      process.exit(1)
+    }
+    // console.log(__dirname, __filename)
+    console.log(files)
   })
   .parse(process.argv);
 
